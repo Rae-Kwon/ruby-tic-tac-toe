09ae27dd03e917b8ac4b1d78c27c0900defa0f1d
Richard Kwon <rkminjae@gmail.com>
2021-01-26 00:46:08 +1300

    Remove initialize method

--- board.rb
+++ board.rb
@@ -6,10 +6,6 @@
 
 # Creates the game board for tic-tac-toe
 class Board
-  def initialize(board_size)
-    @board_size = board_size
-  end
-
   def create_board
     { 'A' => { 1 => '___|', 2 => '___|', 3 => '___' },
       'B' => { 1 => '___|', 2 => '___|', 3 => '___' },
