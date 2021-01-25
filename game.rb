c31cdcebb642a1acef93055c60a484ba414b9bc8
Richard Kwon <rkminjae@gmail.com>
2021-01-26 00:54:07 +1300

    Add 2 Player objects

--- game.rb
+++ game.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 require_relative 'board'
+require_relative 'player'
 
-new_game = Board.new(3)
-new_game.create_board
+new_game = Board.new
+p1 = Player.new(1)
+p2 = Player.new(2)
+
+new_game
+  .create_board
+  .each do |row, col|
+    board_segments = col.values
+    puts board_segments.join
+  end
