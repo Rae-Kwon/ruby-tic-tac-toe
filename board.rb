3e79c532c7b5fe7cf3cf984c5852d1ad1717b8df
Richard Kwon <rkminjae@gmail.com>
2021-01-26 00:27:38 +1300

    Change create_board to be a 2D hash

--- board.rb
+++ board.rb
@@ -4,27 +4,20 @@
 # ___|___|___
 #    |   |
 
+# Board should be 2D array
+# [[A1, A2, A3],
+# [B1, B2, B3],
+# [C1, C2, C3]]
+
 # Creates the game board for tic-tac-toe
 class Board
   def initialize(board_size)
     @board_size = board_size
   end
 
-  private
-
-  def repeat_string(amount = @board_size)
-    amount.times do |line|
-      if line < amount - 1
-        puts '___|___|___'
-      else
-        puts '   |   |   '
-      end
-    end
-  end
-
-  public
-
   def create_board
-    repeat_string
+    { 'A' => { 'A1' => '___|', 'A2' => '___|', 'A3' => '___' },
+      'B' => { 'B1' => '___|', 'B2' => '___|', 'B3' => '___' },
+      'C' => { 'C1' => '   |', 'C2' => '   |', 'C3' => '   ' } }
   end
 end
