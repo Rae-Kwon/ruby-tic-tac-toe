46dca32b0f44feb3b219262624f3a247d4c6bf56
Richard Kwon <rkminjae@gmail.com>
2021-01-26 00:03:44 +1300

    Add methods to get player input

--- player.rb
+++ player.rb
@@ -1,2 +1,22 @@
 # Player class for player inputs
+# player variable must be a number, usually 1 and 2
+class Player
+  def initialize(player)
+    @player = player
+  end
+
+  private
+
+  def players_turn
+    puts "Player #{@player}'s turn"
+  end
+
+  public
+
+  def player_input
+    players_turn
+    input = gets
+    puts "Player #{@player} selected #{input.chomp}"
+  end
+end
 
